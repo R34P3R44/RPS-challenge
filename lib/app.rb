9107c54b0b4ef0a7460bfbf27fb0a91189966a32
Adrian.T <t.adrian88@gmail.com>
2020-10-19 02:19:00 +0100

    Started the game page, failed to display players

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,4 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'shotgun'
 
 class RockPaperScissors < Sinatra::Base
@@ -8,14 +8,36 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/players-reg' do
+    session[:player] = params[:Player]
+    session[:bot] = params[:Bot]
+    puts params
     erb(:players_reg)
     #redirect '/play'
   end
 
-  get '/play' do
-
+  post '/play' do
+    @player = session[:player]
+    @bot = session[:bot]
+    erb :play
   end
 end
+
+  # puts "Player 1 turn"
+  # p1 = gets.chomp
+  # puts "Player 2 turn"
+  # p2 = gets.chomp
+
+  # if p1 == "scissors" && bot == "paper"
+  #   puts "Player 1 wins"
+  # elsif p1 == "rock" && bot == "scissors"
+  #   puts "Player 1 wins"
+  # elsif p1 == "paper" && bot == "rock"
+  #   puts "Player 1 wins"
+  # elsif p1 == bot
+  #   puts "It's a draw"
+  # else
+  #   puts "bot wins"
+
 
 
 
